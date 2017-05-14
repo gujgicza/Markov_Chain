62ba6d6e6144a9610821aa62bc25af8cb6e4289e
gujgicza <gujgiczer.anna@gmail.com>
2017-05-14 21:05:18 +0200

    Added comments.

--- Text_Generator/Text_Generator/main.cpp
+++ Text_Generator/Text_Generator/main.cpp
@@ -5,14 +5,15 @@
 #include <cstdlib>
 #include <ctime>
 
+// Adatstruktúra
 using suffix = std::map<std::string, int>;
 using prefix = std::string;
 using markov_chain = std::map<prefix, suffix>;
 
-// Settings
-static int char_length = 20;
-static std::string input_file_name = "test.txt";
-static long text_length = 500;
+// Beállítások
+static int char_length = 5;                         // milyen hosszú karaktersorokat vizsgáljon
+static std::string input_file_name = "alice.txt";   // melyik szöveg stílusát anulja meg
+static long text_length = 2000;                     // milyen hosszú szöveget generáljon
 
 int main() {
     
@@ -20,35 +21,39 @@ int main() {
     std::ifstream is(input_file_name);
     char c; std::string prefix, suffix;
     
-    // Creating the Markov-chain
-    while (suffix.length() < char_length) { is.get(c); suffix.append(&c); }
+    // Markov-lánc megalkotása
+    while (suffix.length() < char_length) { is.get(c); suffix.append(&c); }     // első prefix
     while (is.get(c)) {
         prefix = suffix;
-        suffix.erase(0,1); suffix.append(&c);
-        ++chain[prefix][suffix];
+        suffix.erase(0,1); suffix.append(&c);                                   // csúszóablak eggyel eltolása
+        ++chain[prefix][suffix];                                                // Markov-lánc frissítése
     }
     
-    // Generating text
+    // Szöveg generálás
     std::srand((int)std::time(0));
     int random = rand()%chain.size()+1;
-    int range; std::string next_str;
+    int range;
+    std::string next_str;
     
     auto current = chain.begin();
-    for(int i = 0; i < random; i++) ++current;
+    for(int i = 0; i < random; i++) ++current;      // Első prefix vélétlenszerű kiválasztása
     std::cout << current->first;
     
-    for (int i = 0; i < text_length; i++) {
+    for (int i = 0; i < text_length; i++) {         // Szöveggenerálás a beállított hosszig
         range = 0;
         std::for_each(current->second.begin(), current->second.end(), [&](auto element) {range += element.second;});
-        random = rand()%range +1;
+        random = rand()%range +1;                   // random szám attól függően, hogy a prefix milyen gyakran fordult elő a szövegben
         
         std::for_each(current->second.begin(), current->second.end(), [&, sum = 0](auto element) mutable {
             if(sum < random) next_str = element.first;
-            sum += element.second;});
+            sum += element.second;});               // szuffixek közül a valószínűségeknek megfelelően választ
 
-        current = chain.find(next_str);
+        current = chain.find(next_str);             // beállítja a szuffixet a következő iteráció prefixének
         
-        if(current == chain.end()) for(int i = 0; i < rand()%chain.size()+1; i++) --current;
-        std::cout << current->first.back();
+        if(current == chain.end()) {                // ha nem talált volna következő elemet, random választ
+            random = rand()%chain.size()+1;
+            for(int i = 0; i < random; i++) --current;
+        }
+        std::cout << current->first.back();         // kiírja csak az utolsó karaktert (hiszen csak 1 karakter eltolás különbség van)
     }
 }
