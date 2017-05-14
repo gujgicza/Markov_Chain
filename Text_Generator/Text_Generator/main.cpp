8ccbe8a99c99d26d567b290ff11a385952bb60d2
gujgicza <gujgiczer.anna@gmail.com>
2017-05-14 20:33:37 +0200

    generates text.

--- Text_Generator/Text_Generator/main.cpp
+++ Text_Generator/Text_Generator/main.cpp
@@ -1,48 +1,54 @@
 #include <iostream>
 #include <map>
 #include <fstream>
-#include <memory>
 
-using node = std::map<std::shared_ptr<std::string>, int>;
-using markov_chain = std::map<std::shared_ptr<std::string>, std::shared_ptr<node>>;
+#include <cstdlib>
+#include <ctime>
+
+using suffix = std::map<std::string, int>;
+using prefix = std::string;
+using markov_chain = std::map<prefix, suffix>;
 
 // Settings
-static int char_length = 5;
+static int char_length = 20;
 static std::string input_file_name = "test.txt";
-static long text_length = 1000;
+static long text_length = 500;
 
 int main() {
     
     markov_chain chain;
     std::ifstream is(input_file_name);
-    char c; std::string str;
-    //node current_node;
+    char c; std::string prefix, suffix;
     
     // Creating the Markov-chain
-    for (int i = 0; i < char_length; i++) {
-        is.get(c);
-        str.append(&c);
-    }
-    auto current_node = chain[std::make_shared<std::string>(str)];
-    
+    while (suffix.length() < char_length) { is.get(c); suffix.append(&c); }
     while (is.get(c)) {
-        //node& current_node = chain[shared_str];
-        str.erase(0,1);
-        str.append(&c);
-        auto shared_str = std::make_shared<std::string>(str);
-        ++current_node.get()->at(shared_str);
-        current_node = chain[shared_str];
+        prefix = suffix;
+        suffix.erase(0,1); suffix.append(&c);
+        ++chain[prefix][suffix];
     }
     
     // Generating text
-    auto current = chain.begin();
-    for (int i = 0; i < text_length; i++) {
-        std::cout << current->first.get()->data(); // << std:: endl;
-        //std::cout << current->second.begin()->first.get()->data();
-        //TODO
-        std::shared_ptr<std::string> next_str = current->second.get()->begin()->first;
-        current = chain.find(next_str);
-    }
+    std::srand((int)std::time(0));
+    int random = rand()%chain.size()+1;
+    int range; std::string next_str;
     
-    return 0;
+    auto current = chain.begin();
+    for(int i = 0; i < random; i++) ++current;
+    std::cout << current->first;
+    
+    for (int i = 0; i < text_length; i++) {
+        range = 0;
+        std::for_each(current->second.begin(), current->second.end(), [&](auto element) {range += element.second;});
+        random = rand()%range +1;
+        
+        std::for_each(current->second.begin(), current->second.end(), [&, sum = 0](auto element) mutable {
+            if(sum < random) next_str = element.first;
+            sum += element.second;});
+
+        current = chain.find(next_str);
+        
+        if(current == chain.end()) for(int i = 0; i < rand()%chain.size()+1; i++) --current;
+        std::cout << current->first.back();
+    }
 }
