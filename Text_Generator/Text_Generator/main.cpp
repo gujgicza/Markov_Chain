b75aa6016725f509f3647334237beaca6f0855c6
gujgicza <gujgiczer.anna@gmail.com>
2017-05-14 16:07:43 +0200

    Before rewrite.

--- Text_Generator/Text_Generator/main.cpp
+++ Text_Generator/Text_Generator/main.cpp
@@ -1,55 +1,48 @@
 #include <iostream>
 #include <map>
-#include <set>
 #include <fstream>
 #include <memory>
-#include <algorithm>
-#include <functional>
 
-
-using node = std::pair<std::shared_ptr<std::string>, std::map<std::shared_ptr<std::string>, int>>;
-using markov_chain = std::set<node>;
+using node = std::map<std::shared_ptr<std::string>, int>;
+using markov_chain = std::map<std::shared_ptr<std::string>, std::shared_ptr<node>>;
 
 // Settings
 static int char_length = 5;
-static std::string input_file_name = "alice.txt";
+static std::string input_file_name = "test.txt";
+static long text_length = 1000;
 
-
-
-int main(int argc, const char * argv[]) {
+int main() {
     
     markov_chain chain;
-    
     std::ifstream is(input_file_name);
-    char c; std::string str; std::shared_ptr<std::string> shared_str;
+    char c; std::string str;
+    //node current_node;
     
     // Creating the Markov-chain
     for (int i = 0; i < char_length; i++) {
         is.get(c);
         str.append(&c);
     }
-    shared_str = std::make_shared<std::string>(str);
-    node current_node = std::make_pair(shared_str, std::map<std::shared_ptr<std::string>, int>());
-    chain.insert(current_node);
+    auto current_node = chain[std::make_shared<std::string>(str)];
     
     while (is.get(c)) {
-        str.erase(0);
+        //node& current_node = chain[shared_str];
+        str.erase(0,1);
         str.append(&c);
-        shared_str = std::make_shared<std::string>(str);
-        ++current_node.second[shared_str];
-        
-        {
-            std::for_each(chain.begin(), chain.end(), [&] (node& n) {
-                if (n.first == shared_str)
-                    current_node = n; });
-            current_node = std::make_pair(shared_str, std::map<std::shared_ptr<std::string>, int>());
-        
-        }
-            
+        auto shared_str = std::make_shared<std::string>(str);
+        ++current_node.get()->at(shared_str);
+        current_node = chain[shared_str];
     }
     
     // Generating text
-    
+    auto current = chain.begin();
+    for (int i = 0; i < text_length; i++) {
+        std::cout << current->first.get()->data(); // << std:: endl;
+        //std::cout << current->second.begin()->first.get()->data();
+        //TODO
+        std::shared_ptr<std::string> next_str = current->second.get()->begin()->first;
+        current = chain.find(next_str);
+    }
     
     return 0;
 }
