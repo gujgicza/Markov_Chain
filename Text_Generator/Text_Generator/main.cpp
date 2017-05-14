c914838626ad10d799a9eb482e21d5569fc986fe
gujgicza <gujgiczer.anna@gmail.com>
2017-05-14 02:41:07 +0200

    Initial commit.

---
+++ Text_Generator/Text_Generator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Text_Generator
+//
+//  Created by Anna Gujgiczer on 2017. 05. 14..
+//  Copyright © 2017. Anna Gujgiczer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
